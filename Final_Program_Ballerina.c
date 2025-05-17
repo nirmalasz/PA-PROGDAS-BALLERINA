88832abf9daf30d917d51d7ab5720152267e35cb
nirmalasz <nirmalawilujeng@gmail.com>
2025-05-17 18:18:43 +0700

    Template awal program

---
+++ Final_Program_Ballerina.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <math.h>
+#include <string.h>
+#include <ctype.h>
+
+//function prototype
+
+
+
+
+
+
+
+
+int main(){
+
+
+    return 0;
+}
+
+
+
+
+//function
+
+
+
+
+
