27bcb6de88261e31f4c4edbf124fa334fa0a69cb
nirmalasz <nirmalawilujeng@gmail.com>
2025-05-18 23:48:11 +0700

    Struct untuk job seeker dan umkm

--- Final_Program_Ballerina.c
+++ Final_Program_Ballerina.c
@@ -5,11 +5,41 @@
 #include <ctype.h>
 
 //function prototype
+void calculateMatchScore(dataUMKM *umkm, dataJobSeeker *pekerja);
 
 
+//struct
+typedef enum{
+    design, copywriting, data_engineering, microsoft_office, programming, marketing, data_analysis, writing, digital_security, project_management, customer_relations, problem_solving, communication, adaptability, teamwork, time_management, conflict, creativity, leadership, critical_thinking, negotiation 
+} skill;
 
+typedef union{
+    char tidak_sekolah[10];
+    char SD[20];
+    char SMP[20];
+    char SMA[20];
+    char S1[10];
+    char S2[10];
+    char S3[10];
+} tingkatPendidikan;
 
+typedef struct {
+    char namaPekerja[50];
+    char kotaTinggal[20];
+    tingkatPendidikan pendidikanTerakhir;
+    int skillDipunyai[20];
+    float gajiEkspektasi;
+    char posisiEkspektasi[20];
+} dataJobSeeker;
 
+typedef struct {
+    char namaUMKM[50];
+    char kotaUMKM[20];
+    tingkatPendidikan minimalPendidikan;
+    int skillDibutuhkan[20];
+    float gajiMinimal;
+    char posisiDibutuhkan[20];
+} dataUMKM;
 
 
 
@@ -20,10 +50,10 @@ int main(){
 }
 
 
-
-
 //function
-
+void calculateMatchScore(dataUMKM *umkm, dataJobSeeker *pekerja){
+    
+}
 
 
 
