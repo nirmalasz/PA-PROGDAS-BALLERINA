081f676eb5ecdca01c5fa0431ce36a417d69ffef
nirmalasz <nirmalawilujeng@gmail.com>
2025-05-21 19:55:27 +0700

    Perbaikan beberapa error dan penambahan fitur match score dengan filter

--- Final_Program_Ballerina.c
+++ Final_Program_Ballerina.c
@@ -31,13 +31,15 @@ typedef struct {
     char posisiDibutuhkan[20];
 } dataUMKM;
 
+typedef enum{
+    semua, lokasi, gaji, pendidikan
+} modeFilter;
+
 //function prototype
 void toLowerRemoveSpace(char *skill);
-int calculateMatchScore(dataJobSeeker *pekerja, dataUMKM *umkm);
-void rekomendasiUMKM(dataJobSeeker *pekerja, dataUMKM daftarUMKM[], int jumlahUMKM);
-void rekomendasiPekerja(dataUMKM *umkm, dataJobSeeker daftarPekerja[], int jumlahPekerja);
-
-
+int calculateMatchScore(dataJobSeeker *pekerja, dataUMKM *umkm, modeFilter);
+void rekomendasiUMKM(dataJobSeeker *pekerja, dataUMKM daftarUMKM[], int jumlahUMKM, modeFilter);
+void rekomendasiPekerja(dataUMKM *umkm, dataJobSeeker daftarPekerja[], int jumlahPekerja, modeFilter);
 
 int main(){
 
@@ -65,8 +67,10 @@ void toLowerRemoveSpace(char *skill){
 }
 
 //fungsi hitung match score antara perushaan a dan pekerja b
-int calculateMatchScore(dataJobSeeker *pekerja, dataUMKM *umkm){
+int calculateMatchScore(dataJobSeeker *pekerja, dataUMKM *umkm, modeFilter mode){
     int score = 0;
+
+    if (mode == 0){
     //hitung berdasar skill
     for (int i = 0; i < pekerja->jumlahSkillDipunya; i++)
     {
@@ -82,36 +86,46 @@ int calculateMatchScore(dataJobSeeker *pekerja, dataUMKM *umkm){
             if (strcmp(skillUMKM,skillUser) == 0) score += 10;
         }
     }
-    //hitung berdasar kota yang sama
-    char kotaUser[20], kotaUMKM[20];
-    strcpy(kotaUser, pekerja->kotaTinggal);
-    strcpy(kotaUMKM, umkm->kotaUMKM);
-    toLowerRemoveSpace(kotaUMKM); toLowerRemoveSpace(kotaUser);
-    if (strcmp(kotaUser, kotaUMKM ) == 0) score += 5;
-    
-    //hitung berdasar posisi yang sama
+       //hitung berdasar posisi yang sama
     char posisiDiinginkan[20], posisiDicari[20];
     strcpy(posisiDiinginkan, pekerja->posisiEkspektasi);
     strcpy(posisiDicari, umkm->posisiDibutuhkan);
-    if (strcmp(kotaUser, kotaUMKM ) == 0) score += 10;
+    if (strcmp(posisiDiinginkan, posisiDicari) == 0) score += 10;
+    }
+    
+    if(mode == 0 || mode == 1){
+        //hitung berdasar kota yang sama
+        char kotaUser[20], kotaUMKM[20];
+        strcpy(kotaUser, pekerja->kotaTinggal);
+        strcpy(kotaUMKM, umkm->kotaUMKM);
+        toLowerRemoveSpace(kotaUMKM); toLowerRemoveSpace(kotaUser);
+        if (strcmp(kotaUser, kotaUMKM ) == 0) score += 5;
+    }
 
+    
+
+    if(mode == 0 || mode == 2){
     //hitung berdasar gaji
     if(umkm->gajiMinimal >= pekerja->gajiEkspektasi) score += 10;
+    }
     
-    //hitung berdasar level pendidikan
-    if (pekerja->pendidikanTerakhir == umkm->minimalPendidikan) score += 5;
-    if (pekerja->pendidikanTerakhir >= umkm->minimalPendidikan) score += 10;
-    else score *= 0;
+    if(mode == 0 || mode ==3){
+        //hitung berdasar level pendidikan
+        if (pekerja->pendidikanTerakhir == umkm->minimalPendidikan) score += 5;
+        if (pekerja->pendidikanTerakhir >= umkm->minimalPendidikan) score += 10;
+        else score *= 0;
+    }
+    
 
     return score;
 }
 
 //fungsi rekomendasi umkm bagi jobseeker
-void rekomendasiUMKM(dataJobSeeker *pekerja, dataUMKM daftarUMKM[], int jumlahUMKM){
+void rekomendasiUMKM(dataJobSeeker *pekerja, dataUMKM daftarUMKM[], int jumlahUMKM, modeFilter mode){
     printf("List Rekomendasi UMKM untuk %s:\n", pekerja->namaPekerja);
     for (int i = 0; i < jumlahUMKM; i++)
     {
-        int scoreMatch = calculateMatchScore(pekerja, &daftarUMKM[i]);
+        int scoreMatch = calculateMatchScore(pekerja, &daftarUMKM[i], mode);
         if (scoreMatch >= 20 && scoreMatch < 70)
         {
             printf("> %s : cocok [matching score sebesar %d]\n", daftarUMKM[i].namaUMKM, scoreMatch);
@@ -123,11 +137,11 @@ void rekomendasiUMKM(dataJobSeeker *pekerja, dataUMKM daftarUMKM[], int jumlahUM
 }
 
 //fungsi rekomendasi pekerja bagi umkm
-void rekomendasiPekerja(dataUMKM *umkm, dataJobSeeker daftarPekerja[], int jumlahPekerja){
+void rekomendasiPekerja(dataUMKM *umkm, dataJobSeeker daftarPekerja[], int jumlahPekerja, modeFilter mode){
     printf("List Rekomendasi pekerja untuk usaha %s:\n", umkm->namaUMKM);
     for (int i = 0; i < jumlahPekerja; i++)
     {
-        int scoreMatch = calculateMatchScore(&daftarPekerja[i], umkm);
+        int scoreMatch = calculateMatchScore(&daftarPekerja[i], umkm, mode);
         if (scoreMatch >= 20 && scoreMatch < 70)
         {
             printf("> %s : cocok [matching score sebesar %d]\n", daftarPekerja[i].namaPekerja, scoreMatch);
@@ -137,11 +151,5 @@ void rekomendasiPekerja(dataUMKM *umkm, dataJobSeeker daftarPekerja[], int jumla
         } 
     }
 }
-
-
-
-
-
-
 
 
